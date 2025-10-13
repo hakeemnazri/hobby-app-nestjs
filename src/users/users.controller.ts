7dccb17f7207d08f8812a61a9bce49bb67b39a1b
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-14 06:37:43 +0800

    Added global pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,6 @@ import {
   ParseIntPipe,
   Query,
   DefaultValuePipe,
-  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,7 +16,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
