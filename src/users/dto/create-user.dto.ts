4e4153de8d77e476d9c4aaf053f0fafe48bd2797
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-14 06:28:43 +0800

    Added class validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
