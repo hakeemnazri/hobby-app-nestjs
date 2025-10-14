9e37fa956400a69cf19487a31cc504ecd188f587
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-14 08:51:30 +0800

    Added circular dependency auth-users module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+  login(email: string, password: string) {
+    const user = this.usersService.findOneById('1234');
+
+    return {
+      user: user,
+      email: email,
+      password: password,
+    };
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  isAuthenticated() {
+    return true;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUsersParamDto } from './dto/get-users-param.dto';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
   create(createUserDto: CreateUserDto) {
     console.log(createUserDto);
     return 'This action adds a new user';
@@ -17,6 +22,8 @@ export class UsersService {
     console.log(getUsersParamDto);
     console.log(limit);
     console.log(page);
+    const isAuth = this.authService.isAuthenticated();
+    console.log(isAuth);
     return [
       {
         name: 'John Doe',
