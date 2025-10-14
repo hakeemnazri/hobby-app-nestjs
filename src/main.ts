899c38a851b0b34737b44570c640ac7c550d1d21
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-14 09:21:46 +0800

    Feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * global pipes
+   */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +16,35 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * swagger configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('My NestJS API - Learning')
+    .setDescription(
+      'The NestJS API Learning Decscription - Base URL: http://localhost:3000/api',
+    )
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense(
+      'MIT License',
+      'https://github.com/nestjs/nest/blob/master/LICENSE',
+    )
+    /**
+     * Optional License
+     */
+    // .setLicense('MIT License', 'https://opensource.org/licenses/MIT')
+    /**
+     * Server Configuration to base URL
+     */
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .addTag('NestJS-learning')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,8 +13,10 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUsersParamDto } from './dto/get-users-param.dto';
 import { PatchUserDto } from './dto/patch-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
