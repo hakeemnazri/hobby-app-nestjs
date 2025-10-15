6fcc1ad3c9d1b6f7ab77fc310160a686667cca73
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-15 10:10:04 +0800

    Add: Post schema

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly prisma: PrismaService,
+  ) {}
   createPost() {
     return 'This action adds a new post';
   }
-  constructor(private readonly usersService: UsersService) {}
   findAll(userId: string) {
     const user = this.usersService.findOneById(userId);
     return [
