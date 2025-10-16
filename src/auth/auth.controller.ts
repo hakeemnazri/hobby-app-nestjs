45840b28f50ce661d3d9b0c1e4e3733771825ef9
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-16 21:03:31 +0800

    Feat: refresh token controller & service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { SignInDto } from './dto/sign-in.dto';
+import { Auth } from './decorators/auth.decorator';
+import { AuthType } from './enums/auth-type.enum';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -9,7 +12,16 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.None)
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @Post('refresh-token')
+  @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.None)
+  refreshToken(@Body() RefreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(RefreshTokenDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { HttpCode, HttpStatus, Injectable } from '@nestjs/common';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { Injectable } from '@nestjs/common';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignInProvider } from './providers/sign-in.provider';
+import { RefreshTokensProvider } from './providers/refresh-tokens.provider';
 
 @Injectable()
 export class AuthService {
@@ -9,12 +11,25 @@ export class AuthService {
      * inject SignInProvider
      */
     private readonly signInProvider: SignInProvider,
+
+    /**
+     * inject RefreshTokensProvider
+     */
+    private readonly refreshTokensProvider: RefreshTokensProvider,
   ) {}
 
-  @HttpCode(HttpStatus.OK)
   async signIn(signInDto: SignInDto) {
     const user = await this.signInProvider.signIn(signInDto);
     return user;
+  }
+
+  async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    const { accessToken } =
+      await this.refreshTokensProvider.refreshToken(refreshTokenDto);
+
+    return {
+      accessToken,
+    };
   }
 
   isAuthenticated() {
